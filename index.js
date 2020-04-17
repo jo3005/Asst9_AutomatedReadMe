a397545bfabb53c4bd8fb0d2e8635566289d15db
jo3005 <jo@caswa.com>
2020-04-17 20:49:25 +0800

    Progress to date: Getting input from user. Writing to a new file. Issues: can't get links in toc to work properly

--- index.js
+++ index.js
@@ -3,52 +3,195 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 
 const fileName='output.md';
-const gitusername='jo3005';
+var gitusername='';
 const gitpassword='';
-const gitrepository='';
+var gitrepository='';
+
+//Get Licence options before starting
+const licence_names=axios.get(`https://api.github.com/licenses`)
+        .then(function(res) {
+            const licNames = res.data.map(function(lic) {
+                return lic.name;
+                })
+            return licNames;
+            })
+        .then(function(licNames){
+            inquirer.prompt([
+                {   type: "input",
+                    message: "Enter your GitHub username:",
+                    name: "username",
+                    default: "jo3005"
+                },
+                {
+                    type: "input",
+                    message: "Enter your new repository name:",
+                    name: "repository",
+                    default: "automated_readme"
+                },
+                {
+                    type: "input",
+                    message: "Enter a short title for your project:",
+                    name: "title",
+                    default: "Automated Readme File Generator"
+                },
+                {
+                    type: "input",
+                    message: "Enter a description of your repository:",
+                    name: "description",
+                    default: "This repo contains code that creates a Readme.md file for a new github repository"
+                },
+                {
+                    type: "input",
+                    message: "Enter your installation guidelines:",
+                    name: "installation",
+                    default: "Installation guidelines have not yet been defined."
+                },
+                {
+                    type: "input",
+                    message: "Enter your usage guidelines:",
+                    name: "usage",
+                    default: "Usage guidelines have not yet been defined."
+                },
+                {
+                    type: "list",
+                    message: "Select a licence type:",
+                    name: "licence",
+                    choices: licNames
+                },
+                {
+                    type: "list",
+                    message: "How can users contribute:",
+                    name: "contributing",
+                    choices:["1. No contribution allowed",
+                            "2. As per the Contributor Covenant v2.0",
+                            "3. Other"],
+                    default: "3. Other"
+                },
+                {
+                    type: "input",
+                    message: "Enter the contributing text:",
+                    name: "contributing_text",
+                    when: function(answers){
+                        return(answers.contributing==="3. Other")
+                    }
+                  
+                },
+                {
+                    type: "input",
+                    message: "Enter any test information:",
+                    name: "tests",
+                    default: "No test information is available at this time."
+            
+                },
+                {
+                    type: "input",
+                    message: "Enter any questions text:",
+                    name: "questions",
+                    default:"No question information is available at this time."
+                }
+            ])
+            .then(data => {
+                //console.log(data);
+                gitusername=data.username;
+                gitrepository=data.repository;
+                let cont_text = '';
+                let toc_contents='';
+                function get_cont_text(data){
+                    const whichcontributing = data.contributing;
+                    
+                    const cont_number = data.contributing.substring(0,1);
+                    
+                    switch(cont_number) {
+                        case "1": // no contributing 
+                            return data.contributing.substring(2);
+                            break;
+                        case "2": // 'As per the Contributor Covenant v2.0'
+                            return data.contributing.substring(2) + '. For more details go to ' + 'http://www.contributor-convenant.org';
+                            break;
+                        case "3":
+                            return data.contributing_text;
+                            break;
+                        default:
+                            return 'No contributing information is available at this time';
+                    }
+                    
+                };
+
+                function create_toc(data){
+                    let readmeKeys= '';
+                    Object.keys(data).forEach(function(key){
+                        let newkey=key;
+                        const ignore=['username','title','description','contributing_text','repository']
+                        //console.log(newkey);
+                        if(ignore.indexOf(newkey) == -1){
+                            
+                            readmeKeys=readmeKeys + '\n * [' + newkey.charAt(0).toUpperCase() + newkey.slice(1) + '] (' + newkey + ')';
+                        }
+                        
+                  })
+                  return readmeKeys;
+                };
+
+                cont_text=get_cont_text(data);
+                toc_contents = create_toc(data);
+
+                console.log(toc_contents);
+
+                const readmeData={
+                    title:'#'+ data.title +' \n',
+                    description: '##Description ' +' \n'+ data.description +' \n',
+                    contents: '##Table of Contents ' +' \n'+ toc_contents +' \n',
+                    installation: '##Installation <a name="installation"></a>'+'\n'+ data.installation +' \n',
+                    usage:'##Usage <a name="usage"></a>'+'\n'+ data.usage +' \n',
+                    license: '##Licence <a name="licence"></a>'+'\n'+ data.licence +' \n',
+                    contributing: '##Contributing <a name="contributing"></a>'+' \n'+ cont_text +' \n',
+                    tests: '##Tests <a name="tests"></a>'+'\n'+ data.tests +' \n',
+                    questions: '##Questions <a name="questions"></a>'+' \n'+ data.questions +' \n'
+                }; 
+                //console.log(readmeData);
+                return readmeData;
+              })
+              .then(data => {
+                 //console.log(data);
+                  let readmeText= '';
+                  Object.values(data).forEach(function(value){
+                    const newtext=value;
+                    console.log(newtext);
+                    readmeText=readmeText + '\n' + newtext;
+                  })
+                  
+                  return readmeText;
+              })
+              .then(readmeText => {
+                fs.writeFile(fileName, readmeText, function(err) {
+
+                    if (err) {
+                      return console.log(err);
+                    }
+                  
+                    console.log("New file created!");
+                  
+                  });
+              })
+              .catch(error => {
+                console.log(error)
+              });
 
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-    axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
 
-      const repoNamesStr = repoNames.join("\n");
-
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
-
-        console.log(`Saved ${repoNames.length} repos`);
-      });
-    });
-  });
+        }) 
+    ; 
 
 
 
-const readme={
-    filename: fileName,
-    title:'#'+ process.argv[2] +'\n',
-    description: '#Description' +'\n'+ ''+'\n',
-    contents: '#Table of Contents' +'\n'+ ''+'\n',
-    installation: '#Installation'+'\n'+ ''+'\n',
-    usage:'#Usage'+'\n'+ ''+'\n',
-    license: '#Licence'+'\n'+ ''+'\n',
-    contributing: '#Contributing'+'\n'+ ''+'\n',
-    tests: '#Tests'+'\n'+ ''+'\n',
-    questions: '#Questions'+'\n'+ ''+'\n'
-}
 
 
+
+
+
+
+/* 
 // Create new readme file
 fs.writeFile(readme.filename, '', function(err) {
 
@@ -81,7 +224,7 @@ fs.appendFile(readme.filename,readme.contents,function(err){
     
       console.log("Table of Contents added");
 });
-
+ */
 
 
 
--- output.md
+++ output.md
@@ -1,5 +1,32 @@
-#Description
 
-#Table of Contents
+#Automated Readme File Generator 
 
-#newtitle
+##Description  
+This repo contains code that creates a Readme.md file for a new github repository 
+
+##Table of Contents  
+
+ * [Installation] (installation)
+ * [Usage] (usage)
+ * [Licence] (licence)
+ * [Contributing] (contributing)
+ * [Tests] (tests)
+ * [Questions] (questions) 
+
+##Installation <a name="installation"></a>
+Installation guidelines have not yet been defined. 
+
+##Usage <a name="usage"></a>
+Usage guidelines have not yet been defined. 
+
+##Licence <a name="licence"></a>
+GNU Affero General Public License v3.0 
+
+##Contributing <a name="contributing"></a> 
+ 
+
+##Tests <a name="tests"></a>
+No test information is available at this time. 
+
+##Questions <a name="questions"></a> 
+No question information is available at this time. 
